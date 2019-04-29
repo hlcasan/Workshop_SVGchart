568e538062f4fe320ed2e403c2180e4713eb17e1
Hugo L. Casanova <casanovh@vanier.college>
2019-04-29 12:29:58 -0400

    Minor change

--- SVGchart/php/db_connect.php
+++ SVGchart/php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 //Establish connection: host, user, password, database
-$dbi = mysqli_connect("localhost","PIApp","","HugoTMP");
+$dbi = mysqli_connect("localhost","*****","*****","HugoTMP");
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
